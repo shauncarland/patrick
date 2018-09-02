05f530d278f1950f3fcbb2cd597f1e65d4c34307
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-09-02 14:45:53 -0400

    sass

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+      <div className="parent">
+        parent
+        <div className="child">Child</div>
       </div>
+    </div>
     );
   }
 }
