208f6f6b2c737960224b15dcf871bbf997b416af
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-09-03 00:35:02 -0400

    fix the rest of the links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
 class Navbar extends Component {
   myFunction = function() {
     var x = document.getElementById("myTopnav");
+
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
   }
+
   render() {
     return(
       <div className="topnav" id="myTopnav">
       <Link to={`${process.env.PUBLIC_URL}/`}>Home</Link>
       <Link to={`${process.env.PUBLIC_URL}/about`}>Bio</Link>
-          <a href="/projects">Projects</a>
-          <a href="/hire">LOL</a>
-          <a href="/contact">Contact</a>
+      <Link to={`${process.env.PUBLIC_URL}/projects`}>Projects</Link>
+      <Link to={`${process.env.PUBLIC_URL}/hire`}>Hire Me</Link>
+      <Link to={`${process.env.PUBLIC_URL}/contact`}>Contact</Link>
           <a href="javascript:void(0);" className="icon" onClick={this.myFunction.bind(this)}>
             <i className="fa fa-bars"></i>
           </a>
